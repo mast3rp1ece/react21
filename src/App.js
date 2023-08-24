1b92e938b59c4d83b5fffbb633dc44eb8236dd59
Taras Kotovych <t.kotovych@gmail.com>
2023-08-24 18:31:32 +0300

    change start page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="App">
           <Header />
           <Routes>
-            <Route path='/posts' element={ <Posts />} />
+            <Route path='*' element={ <Posts />} />
             <Route path='/addpost' element={<AddPost />} />
           </Routes>
         </div>
